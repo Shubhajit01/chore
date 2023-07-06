935b157a8b44e2b4e81fffd3fd8fae363d757ea7
Shubhajit01 <shubhajitchatterjee1997@gmail.com>
2023-07-06 09:46:16 +0530

    Updated route

---
+++ app/routes/dash.board/route.tsx
@@ -0,0 +1,67 @@
+import { LoaderArgs, V2_MetaFunction, json } from "@remix-run/cloudflare";
+import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { TypographyH5 } from "~/components/ui/typography";
+import DATA from "~/constants/data";
+import getDB from "~/db";
+import { cn } from "~/lib/utils";
+
+export async function loader({ context }: LoaderArgs) {
+  const db = await getDB(context.env.DB);
+
+  return json(
+    await db.query.users.findFirst({
+      columns: { id: true },
+      where: (users, { eq }) => eq(users.email, "shubhajit@chore.com"),
+      with: {
+        boards: {
+          columns: { name: true, slug: true, updatedAt: true },
+          orderBy: (board, { desc }) => [desc(board.updatedAt)],
+        },
+      },
+    })
+  );
+}
+
+export const meta: V2_MetaFunction = () => [
+  { title: `Boards - ${DATA.APP_TITLE}` },
+];
+
+export default function BoardLayout() {
+  const { boards } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <div className="hidden w-[18rem] shrink-0 overflow-hidden border-r border-gray-800/40 bg-black/10 py-6 px-8 duration-100 lg:block">
+        <div className="mt-5 mb-3">
+          <TypographyH5>
+            Boards (<span>{boards.length}/10</span>)
+          </TypographyH5>
+        </div>
+
+        <nav>
+          <ul className="space-y-1">
+            {boards.map((board) => {
+              return (
+                <li key={board.slug} className="py-0.5 text-slate-300">
+                  <NavLink
+                    prefetch="intent"
+                    to={`/dash/board/${board.slug}`}
+                    className={({ isActive }) =>
+                      cn(
+                        "flex items-center gap-2 truncate font-medium",
+                        isActive ? "text-sky-400" : "hover:text-white"
+                      )
+                    }
+                  >
+                    {board.name}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
+      <Outlet />
+    </>
+  );
+}
---
+++ app/routes/dash.tsx
@@ -0,0 +1,53 @@
+import { NavLink, Outlet } from "@remix-run/react";
+import { KanbanIcon, UserCircle, LogOutIcon } from "lucide-react";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { cn } from "~/lib/utils";
+
+export default function DashRoute() {
+  const navLinks = [
+    { label: "Home", icon: BiHomeAlt2, to: "/" },
+    { label: "Boards", icon: KanbanIcon, to: "/dash/board" },
+    { label: "Profile", icon: UserCircle, to: "/dash/me" },
+  ];
+
+  return (
+    <main className="flex min-h-screen w-screen flex-col lg:h-screen lg:flex-row">
+      <aside className="hidden lg:flex w-20 pt-11 pb-6 flex-col bg-black/30">
+        <div className="px-6 grow-0">
+          <img
+            src="/icon-logo.svg"
+            width="30"
+            height="27"
+            alt="A slanted grid symbol is displayed on the left side, featuring three columns with sky, white, and sky background colors respectively."
+          />
+        </div>
+
+        <ul className="flex flex-col grow items-center gap-5 mt-10">
+          {navLinks.map((link) => (
+            <li key={link.to} className="w-full">
+              <NavLink
+                to={link.to}
+                className={({ isActive }) =>
+                  cn(
+                    "flex flex-col w-full gap-0.5 items-center",
+                    isActive ? "text-white" : "text-slate-500"
+                  )
+                }
+              >
+                <link.icon className="w-6 h-6" />
+                <small className="text-xs font-medium">{link.label}</small>
+              </NavLink>
+            </li>
+          ))}
+
+          <li className="mt-auto flex w-full flex-col gap-0.5 items-center text-rose-400">
+            <LogOutIcon className="w-6 h-6" />
+            <small className="text-xs font-medium">Logout</small>
+          </li>
+        </ul>
+      </aside>
+
+      <Outlet />
+    </main>
+  );
+}
